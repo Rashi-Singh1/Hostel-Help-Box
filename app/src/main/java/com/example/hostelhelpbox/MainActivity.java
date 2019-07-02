2a8c0150e335dbaf00c5bb604e644b57e7a74cf5
Rashi Singh <rashi170101052@iitg.ac.in>
2019-07-02 21:40:36 +0530

    MainActivity changes

--- app/src/main/java/com/example/hostelhelpbox/MainActivity.java
+++ app/src/main/java/com/example/hostelhelpbox/MainActivity.java
@@ -1,8 +1,26 @@
+//package com.example.hostelhelpbox;
+//
+//import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Intent intent = new Intent(getApplicationContext(), Login.class);
+//        startActivity(intent);
+//    }
+//}
 package com.example.hostelhelpbox;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +28,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(getApplicationContext(), Login.class);
-        startActivity(intent);
+        Button Login = (Button) findViewById(R.id.btnLogin);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
+        });
+        Button Signup = (Button) findViewById(R.id.btnSignup);
+        Signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
