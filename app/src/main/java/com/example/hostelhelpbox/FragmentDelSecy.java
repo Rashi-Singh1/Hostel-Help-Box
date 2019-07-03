ba408855ecf34218d24608a1a158d36f1b8faa12
Rashi Singh <rashi170101052@iitg.ac.in>
2019-07-04 02:48:27 +0530

    Color n trash icon to delete added

--- app/src/main/java/com/example/hostelhelpbox/FragmentDelSecy.java
+++ app/src/main/java/com/example/hostelhelpbox/FragmentDelSecy.java
@@ -1,11 +1,16 @@
 package com.example.hostelhelpbox;
 
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
@@ -33,6 +38,8 @@ public class FragmentDelSecy extends Fragment {
     {
 
     }
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -102,7 +109,7 @@ public class FragmentDelSecy extends Fragment {
         @Override
         public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
             int j = viewHolder.getAdapterPosition();
-            Toast.makeText(getContext(),"index j : "+ Integer.toString(j),Toast.LENGTH_SHORT).show();
+//            Toast.makeText(getContext(),"index j : "+ Integer.toString(j),Toast.LENGTH_SHORT).show();
 //            Toast.makeText(getContext(),"index getpositionnnn : "+ Integer.toString(viewHolder.getAdapterPosition()),Toast.LENGTH_SHORT).show();
             DatabaseReference ref;
             ref = FirebaseDatabase.getInstance().getReference("Users/"+list.get(j).getUsername());
@@ -115,6 +122,46 @@ public class FragmentDelSecy extends Fragment {
             Snackbar.make(viewHolder.itemView, removedItem.getUsername()+" deleted",Snackbar.LENGTH_LONG).setAction("UNDO", new MyUndoListener()).show();
 //  Adapter.notifyDataSetChanged();
         }
+
+        @Override
+        public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+                // Get RecyclerView item from the ViewHolder
+                View itemView = viewHolder.itemView;
+                int indianRed = Color.rgb(205,92,92);
+                int raspberry = Color.rgb(210,31,60);
+
+                int persian = Color.rgb(202,52,51);
+                int redwood = Color.rgb(164,90,82);
+                int desire = Color.rgb(234,60,83);
+                int salmon = Color.rgb(250,128,114);
+                Drawable trash = ContextCompat.getDrawable(getContext(),R.drawable.ic_delete_black_24dp);
+
+                int iconMargin = (itemView.getHeight()-trash.getIntrinsicHeight())/2;
+//                int iconMargin = (itemView.getHeight()-trash.getIntrinsicHeight())/2;
+                Paint p = new Paint();
+                if (dX > 0) {
+                    /* Set your color for positive displacement */
+                    p.setColor(salmon);
+                    trash.setBounds(itemView.getLeft()+iconMargin,itemView.getTop()+iconMargin,itemView.getLeft()+iconMargin+trash.getIntrinsicWidth(),itemView.getBottom()-iconMargin);
+                    // Draw Rect with varying right side, equal to displacement dX
+                    c.drawRect((float) itemView.getLeft(), (float) itemView.getTop(), dX,
+                            (float) itemView.getBottom(), p);
+                } else {
+                    /* Set your color for negative displacement */
+                    p.setColor(salmon);
+                    trash.setBounds(itemView.getRight()-iconMargin-trash.getIntrinsicWidth(),itemView.getTop()+iconMargin,itemView.getRight()-iconMargin,itemView.getBottom()-iconMargin);
+
+                    // Draw Rect with varying left side, equal to the item's right side plus negative displacement dX
+                    c.drawRect((float) itemView.getRight() + dX, (float) itemView.getTop(),
+                            (float) itemView.getRight(), (float) itemView.getBottom(), p);
+                }
+                trash.draw(c);
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        }
+
     };
 
     class MyUndoListener implements View.OnClickListener {
@@ -127,4 +174,7 @@ public class FragmentDelSecy extends Fragment {
             Adapter.notifyDataSetChanged();
         }
     }
+
+
+
 }
