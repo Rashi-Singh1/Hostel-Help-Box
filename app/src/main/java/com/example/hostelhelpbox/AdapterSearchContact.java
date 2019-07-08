bbe1d618ca9966a1ce003aa07a47600cd17a51d8
Rashi Singh <rashi170101052@iitg.ac.in>
2019-07-08 13:48:34 +0530

    Search working

--- app/src/main/java/com/example/hostelhelpbox/AdapterSearchContact.java
+++ app/src/main/java/com/example/hostelhelpbox/AdapterSearchContact.java
@@ -6,18 +6,21 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class AdapterSearchContact extends RecyclerView.Adapter<AdapterSearchContact.MyViewHolder> {
+public class AdapterSearchContact extends RecyclerView.Adapter<AdapterSearchContact.MyViewHolder> implements Filterable{
     Context context;
     ArrayList<User> mytodos;
-
+    private ArrayList<User> searchList;
     public AdapterSearchContact(Context c, ArrayList<User> p)
     {
         context = c;
         mytodos = p;
+        searchList = new ArrayList<>(p);
     }
 
     @NonNull
@@ -54,4 +57,41 @@ public class AdapterSearchContact extends RecyclerView.Adapter<AdapterSearchCont
             secyof = (TextView) itemView.findViewById(R.id.secyof);
         }
     }
+
+    @Override
+    public Filter getFilter() {
+        return myFilter;
+    }
+
+    private Filter myFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            ArrayList<User> filteredList = new ArrayList<>();
+            if(constraint == null || constraint.length() == 0)
+            {
+                filteredList.addAll(searchList);
+            }
+            else{
+                String filterpattern = constraint.toString().toLowerCase().trim();
+
+                for (User item: searchList) {
+                    if(item.getFullName().toLowerCase().contains(filterpattern) || item.getHostel().toLowerCase().contains(filterpattern) || item.getSecyOf().toLowerCase().contains(filterpattern))
+                    {
+                        filteredList.add(item);
+                    }
+                }
+            }
+
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            mytodos.clear();
+            mytodos.addAll((ArrayList) results.values);
+            notifyDataSetChanged();
+        }
+    };
 }
--- app/src/main/java/com/example/hostelhelpbox/SearchContact.java
+++ app/src/main/java/com/example/hostelhelpbox/SearchContact.java
@@ -7,8 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,7 +34,9 @@ public class SearchContact extends AppCompatActivity {
     ArrayList<User> list;
     DatabaseReference ref;
     RecyclerView viewFeedback;
+    AdapterSearchContact Adapter;
     TextView title, endpage;
+//    EditText search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,24 +47,40 @@ public class SearchContact extends AppCompatActivity {
 
         title = findViewById(R.id.title);
         endpage = findViewById(R.id.endpage);
+//        search = findViewById(R.id.search);
         viewFeedback = findViewById(R.id.viewFeedback);
         viewFeedback.setLayoutManager(new LinearLayoutManager(this));
         list = new ArrayList<>();
+//        search.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_SHORT).show();
+//                Adapter.getFilter().filter(s);
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//
+//            }
+//        });
+
         ref = FirebaseDatabase.getInstance().getReference().child("Users");
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
                 for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
-                    //Toast.makeText(getApplicationContext(),"getting or not",Toast.LENGTH_LONG).show();
                     User p = dataSnapshot1.getValue(User.class);
                     list.add(p);
-                    //Toast.makeText(getApplicationContext(),p.getQuesName(),Toast.LENGTH_LONG).show();
-
                 }
                 if (list.size() != 0)
                 {
-                    AdapterSearchContact Adapter = new AdapterSearchContact(getApplicationContext(),list);
+                    Adapter = new AdapterSearchContact(getApplicationContext(),list);
                     viewFeedback.setAdapter(Adapter);
                     Adapter.notifyDataSetChanged();
                 }
@@ -69,4 +94,24 @@ public class SearchContact extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu,menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
+    }
 }
